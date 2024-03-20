1a7e2b41c8a84e8b8ca896b6f218dfe25743a2dd
DwarfPT <eduard.dimitriu@mail.ru>
2024-03-20 03:08:41 +0200

    Adding Basic Bussines Logic

---
+++ ShopFurniture.2.Domain/Entities/Auth/UCoockieData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopFurniture._2.Domain.Entities.Auth
+{
+    public class UCoockieData
+    {
+        public int MaxAge { get; set; }
+        public string Coockie { get; set; }
+    }
+}
